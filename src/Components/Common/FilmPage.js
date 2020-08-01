b9a7275054186dbc8dd0ef0f40d831fdc63ce71f
serhii <lashchivskyi@gmail.com>
2020-08-01 23:38:12 +0300

    added search, improved existing logic

--- src/Components/Common/FilmPage.js
+++ src/Components/Common/FilmPage.js
@@ -5,13 +5,22 @@ import FilmRecommendation from "./RecommendationFilms";
 import style from "./FilmPage.module.css";
 
 class FilmPage extends React.Component {
-  componentDidMount = () => {
+  refreshProfile() {
     this.props.getPostsAction(this.props.match.params.id);
+  }
+
+  componentDidMount = () => {
+    this.refreshProfile();
   };
-  movieId = 0;
+
+  componentDidUpdate(prevProps, prevState) {
+    window.scrollTo(0, 0);
+    if (this.props.match.params.id !== prevProps.match.params.id)
+      this.refreshProfile();
+  }
+
   render = () => {
-    const { loaded, movie } = this.props;
-    this.movieId = this.props.match.params.id;
+    const { loaded, movie, match } = this.props;
     return (
       <>
         <div className={style.blockFilm}>
@@ -20,23 +29,23 @@ class FilmPage extends React.Component {
             {!loaded ? (
               <h2> Loading ... </h2>
             ) : (
-                <div className={style.filmContainer}>
-                  <img
-                    className={style.imgPhoto}
-                    alt="poster"
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  ></img>
-                  <div className={style.descriptionFilm}>
-                    <h3>{movie.tagline}</h3>
-                    <h4>{movie.original_title}</h4>
-                    <h5>{movie.overview}</h5>
-                    <div>{movie.release_date}</div>
-                    <div>{movie.popularity}</div>
-                  </div>
+              <div className={style.filmContainer}>
+                <img
+                  className={style.imgPhoto}
+                  alt="poster"
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                ></img>
+                <div className={style.descriptionFilm}>
+                  <h3>{movie.tagline}</h3>
+                  <h4>{movie.original_title}</h4>
+                  <h5>{movie.overview}</h5>
+                  <div>{movie.release_date}</div>
+                  <div>{movie.popularity}</div>
                 </div>
-              )}
+              </div>
+            )}
           </div>
-          <FilmRecommendation />
+          <FilmRecommendation movieId={match.params.id} />
         </div>
       </>
     );
@@ -52,6 +61,9 @@ const mapDispatchToProps = (dispatch) => ({
   getPostsAction: (movieId) => {
     dispatch(getMovie(movieId));
   },
+  // getPostsRecomAction: (movieRecomId) => {
+  //   dispatch(getMovieRecommendationFilms(movieRecomId));
+  // },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(FilmPage);
